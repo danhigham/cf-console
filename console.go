35220ecce95d67bfbcedcccff53cd96d5bb211b4
Dan Higham <dan.higham@gmail.com>
2015-01-16 14:13:50 -0800

    Fix staging_info.yml path

--- console.go
+++ console.go
@@ -59,7 +59,7 @@ func (plugin ConsolePlugin) Run(cliConnection plugin.CliConnection, args []strin
 	guid, entity := plugin.FindAppGuid(cliConnection, appName)
 
 	// Update the app to start tmate
-	plugin.UpdateForTmate(cliConnection, guid, "")
+	plugin.UpdateForTmate(cliConnection, guid, "sleep 300")
 
 	// Kill the first instance
 	// plugin.KillInstanceZero(cliConnection, guid)
